be4b2ca9eedd5f37939ea75f08e9d831f7c05a8e
x-info <dmsadikins@gmail.com>
2025-02-26 12:57:57 +0700

    update Sidebar Colors

--- resources/js/Components/ArgonSidebarColors.vue
+++ resources/js/Components/ArgonSidebarColors.vue
@@ -1,6 +1,6 @@
 
 <script setup>
-import { computed } from 'vue';
+import { computed, onUpdated } from 'vue';
 
 
     const props = defineProps( {
@@ -9,7 +9,15 @@ import { computed } from 'vue';
     isActive: {type:Boolean}
     } )
 
-    const classes = computed(() =>  props.isActive ? 'border-slate-700 dark:border-white' : 'border-white dark:border-slate-700')
+    const classes = computed( () => props.isActive ? 'border-slate-700 dark:border-white' : 'border-white dark:border-slate-700' )
+
+    const grayDarkClasses = computed( () => props.from === 'zinc-800' ? 'dark:from-slate-750 dark:to-gray-850' : '' )
+
+    onUpdated( () =>
+    {
+        console.log( 'isActive ', props.isActive)
+
+    })
 
 
 
@@ -22,7 +30,7 @@ import { computed } from 'vue';
 
 
     <span
-    :class="` ${classes} py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-${props.from} to-${props.to} dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700`"></span>
+    :class="` ${classes} py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-${props.from} to-${props.to} dark:bg-gradient-to-tl ${grayDarkClasses} relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700`"></span>
 
 
 </template>
